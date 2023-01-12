7c94c883cee3212472f0d7be716d2925ef5ef5ab
youngsuper <1406422800@qq.com>
2023-01-13 02:24:24 +0800

    copy src

--- vite.base.config.js
+++ vite.base.config.js
@@ -2,10 +2,12 @@
  * @Author: youngsuper 1406422800@qq.com
  * @Date: 2023-01-13 02:12:15
  * @LastEditors: youngsuper 1406422800@qq.com
- * @LastEditTime: 2023-01-13 02:17:30
+ * @LastEditTime: 2023-01-13 02:20:53
  * @FilePath: /vue-document/vite.base.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+import { fileURLToPath, URL } from "node:url";
+
 import { defineConfig } from "vite";
 
 import vue from '@vitejs/plugin-vue';
